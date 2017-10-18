2ac5df363b9a988c1e29cebef0516f792f4306f8
Seale Rapolai <1098005@students.wits.ac.za>
2017-10-18 21:48:46 +0200

    input files.

--- cluster_input.py
+++ cluster_input.py
@@ -3,6 +3,11 @@ import sys
 
 num_clusters = int(input("Enter number of clusters: ")) # k
 num_points = int(input("Enter number of points per cluster: ")) # number of points in each cluster
+dim = int(input("Enter set dimension: "))
+
+k_means_input = open("test1.txt","w")
+k_means_input.write(str(num_clusters*num_points) + " " + str(dim) + " " + str(num_clusters))
+k_means_input.close()
 
 file = open("input.txt","w")
 #file.write(str(num_points) + "\n")
@@ -14,8 +19,11 @@ variance = ""
 for i in range(num_clusters):
 	vari = random.uniform(1, 10)
 	variance += str(vari) + " "
-	x = random.uniform(-vari, vari)*5
-	y = random.uniform(-vari, vari)*5
-	file.write(str(x) + " " + str(y) + "\n")
+
+	for j in range(dim):
+		point = random.uniform(-vari*5, vari*5)
+		file.write(str(point) + " ")
+	
+	file.write("\n")
 
 file.write(variance)
